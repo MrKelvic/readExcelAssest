7ebf37dd7bb22c55f0cbc3f1ca22ea0c8e952a3a
MrKelvic <kelvicansa1@gmail.com>
2022-04-27 15:51:36 +0000

    added read function and updated dict keys

--- index.py
+++ index.py
@@ -16,14 +16,21 @@ def genDataTemplate():
 def removeNewLine(string):
     return str(string).strip()
 
+def readData(data):
+    for data_value in data:
+        # print(data_value.items())
+        for attr, value in data_value.items():
+            print("%s : %s" %(attr,", ".join(value)))
+        print('-'*25)
+
 def main():
     data=[genDataTemplate()]
-    indexControllers=["source","device","destination","port"] #controls which dictionary to add cell data to
+    indexControllers=["sources","devices","destinations","ports"] #controls which dictionary to add cell data to
     wb = lib.load_workbook("./test_acl.xlsx",read_only=False)
     sheet = wb.worksheets[0]
     x=sheet.max_column
     y=sheet.max_row
-    print("No. Rows: %s No. Col %s" %(y,x))
+    # print("No. Rows: %s No. Col %s" %(y,x))
     for rowList in sheet.iter_rows(): #loop through row elements
         for xIndex in range(0,x): #loop every colomn in the row
             if rowList[xIndex].internal_value:
@@ -34,7 +41,7 @@ def main():
         #done with row, create append a new dict to data
         data.append(genDataTemplate()) #remove first row
     data.pop(0)
-    print(data)
+    readData(data)
     return
 
 if __name__ == '__main__':
